4b6bc2fa1ea3fb5a59e9772143e4ec68cf976607
nicolae2551 <panait.nicolae.2001@gmail.com>
2025-11-12 13:06:23 -0500

    update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,6 +40,7 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
